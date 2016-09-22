b9cdfba96eec023a1d680a5eadb24bd3c8017afe
davidblancolopez <daviddbl94@gmail.com>
2016-09-22 17:57:07 +0200

    lalala

--- app/src/main/java/com/example/alumnedam/aboutrobots/RobotsActivity.java
+++ app/src/main/java/com/example/alumnedam/aboutrobots/RobotsActivity.java
@@ -1,45 +1,62 @@
 package com.example.alumnedam.aboutrobots;
 
+import android.content.pm.ActivityInfo;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RobotsActivity extends AppCompatActivity implements View.OnClickListener {
+    private int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_robots);
 
         Button buttonnotocar = (Button) findViewById(R.id.buttonnotocar);
+        //Creación del botón (buttonnotocar).
         buttonnotocar.setOnClickListener(this);
+        //Assignación de un OnClickListener en este botón.
 
+        ImageButton imageButtonSalir = (ImageButton) findViewById(R.id.imageButtonSalir);
+        imageButtonSalir.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         //View v és el boton que se ha pulsado.
-        int contador = 0;
         if(v.getId() == R.id.buttonnotocar){
             //comprobación para detectar que el boton pulsado és el que inicia este metodo.
             if(contador == 0) {
                 Button buttonnotocar = (Button) v;
-                buttonnotocar.setText("Que no me toques!!!");
+
+                buttonnotocar.setText(R.string.textoBoton2);
 
                 TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText("Parece que los humanos no són muy inteligntes.");
+                textView.setText(R.string.textoMensaje2);
                 contador ++;
-            }
-            if(contador == 2){
+            }else if (contador > 0){
+
                 Button buttonnotocar = (Button) v;
                 buttonnotocar.setVisibility(View.INVISIBLE);
-                contador ++;
+                ImageView imagen = (ImageView) findViewById(R.id.imagen);
+                imagen.setImageResource(R.drawable.explosion);
+
+            }else if(v.getId() == R.id.imageButtonSalir){
+                finish();
             }
         }
-
-
     }
+
 }
+
+
